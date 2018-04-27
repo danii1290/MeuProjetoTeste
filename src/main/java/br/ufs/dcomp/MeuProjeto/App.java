a625ff6b41a9cf241e946ace6d0673e97bd8b98b
dani <dsantos@dcomp.ufs.br>
2018-04-27 19:40:42 +0000

    alterado

--- src/main/java/br/ufs/dcomp/MeuProjeto/App.java
+++ src/main/java/br/ufs/dcomp/MeuProjeto/App.java
@@ -1,3 +1,4 @@
+
 package br.ufs.dcomp.MeuProjeto;
 
 /**
@@ -8,6 +9,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Teste!" );
     }
 }
